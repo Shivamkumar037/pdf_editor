d9e89b00047cc076f54efc6a96a4b9265e9c72cf
Shivam Kumar <shivamkumar37je@gmail.com>
2025-12-09 02:07:56 +0530

    Update UserController.java

--- backend/src/main/java/com/shivam/fullstack/backend/Controller/UserController.java
+++ backend/src/main/java/com/shivam/fullstack/backend/Controller/UserController.java
@@ -1,11 +1,13 @@
 package com.shivam.fullstack.backend.Controller;
+
 import com.shivam.fullstack.backend.Entity.User;
 import com.shivam.fullstack.backend.Repositories.UserRepository;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.*; // Isme CrossOrigin bhi aa jata hai
 import java.util.List;
 
 @RestController
 @RequestMapping("/userdata")
+@CrossOrigin(origins = "*") // <--- YE LINE ADD KI HAI (CORS Fix)
 public class UserController {
     private final UserRepository repo;
 
