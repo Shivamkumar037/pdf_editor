c3932bcd7394d4cb8d5000c38aad5efbca47ca36
Shivam Kumar <shivamkumar37je@gmail.com>
2025-12-08 22:22:44 +0530

    Add files via upload

---
+++ backend/src/main/java/com/shivam/fullstack/backend/Repositories/UserRepository.java
@@ -0,0 +1,6 @@
+package com.shivam.fullstack.backend.Repositories;
+import com.shivam.fullstack.backend.Entity.Product;
+import com.shivam.fullstack.backend.Entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface UserRepository extends JpaRepository<User, Long> {}
+
