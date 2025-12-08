eddf35e739049919fb0ab0aed1a7598d9936048a
Shivam Kumar <shivamkumar37je@gmail.com>
2025-12-08 09:57:01 -0800

    Update Product.java

--- backend/src/main/java/com/shivam/fullstack/backend/Entity/Product.java
+++ backend/src/main/java/com/shivam/fullstack/backend/Entity/Product.java
@@ -15,5 +15,46 @@ public class Product {
     private String imglink;
     private Double price;
 
-    // getters and setters
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProductname() {
+        return productname;
+    }
+    public void setProductname(String productname) {
+        this.productname = productname;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getProductlink() {
+        return productlink;
+    }
+    public void setProductlink(String productlink) {
+        this.productlink = productlink;
+    }
+
+    public String getImglink() {
+        return imglink;
+    }
+    public void setImglink(String imglink) {
+        this.imglink = imglink;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+    public void setPrice(Double price) {
+        this.price = price;
+    }
 }
