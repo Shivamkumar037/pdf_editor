09d03a2a182b9deceaa67063b94d5001b215e09a
Shivam Kumar <shivamkumar37je@gmail.com>
2025-12-09 11:34:21 +0530

    Update UserRepository.java

--- backend/src/main/java/com/shivam/fullstack/backend/Repositories/UserRepository.java
+++ backend/src/main/java/com/shivam/fullstack/backend/Repositories/UserRepository.java
@@ -1,14 +1,12 @@
 package com.shivam.fullstack.backend.Repositories;
-import com.shivam.fullstack.backend.Entity.Product;
-import com.shivam.fullstack.backend.Entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 
 import com.shivam.fullstack.backend.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    // यह वही मेथड है जो UserController में इस्तेमाल हो रहा है
     User findByMobile(String mobile);
 }
-
