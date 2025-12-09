3973587290224ea90c612b3228a5909bb898dc1a
Shivam Kumar <shivamkumar37je@gmail.com>
2025-12-09 11:35:11 +0530

    Update ProductRepository.java

--- backend/src/main/java/com/shivam/fullstack/backend/Repositories/ProductRepository.java
+++ backend/src/main/java/com/shivam/fullstack/backend/Repositories/ProductRepository.java
@@ -2,6 +2,8 @@ package com.shivam.fullstack.backend.Repositories;
 
 import com.shivam.fullstack.backend.Entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {}
-
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
